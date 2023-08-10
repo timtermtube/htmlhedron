f667c9d2301842811178e5ccbee9e0b5b0989162
timtermtube <timtermtube@gmail.com>
2023-08-11 06:13:53 +0900

    For Bundling

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,16 @@ import {Point, complexKinds, coord} from "./complex";
 import {Camera} from "./camera";
 import {Scene} from "./scene";
 
-namespace HTMLHedron {
-    const scene: Scene = new Scene();
+const scene: Scene = new Scene();
 
-    export async function createPoint(i: {
-        radius: number,
-        color: string,
-        coord: coord
-    }): Promise<Point> {
-        return new Point(i.radius||1, i.color||"#000000", i.coord||[0,0,0]);
-    }
+export async function createPoint(i: {
+    radius: number,
+    color: string,
+    coord: coord
+}): Promise<Point> {
+    return new Point(i.radius||1, i.color||"#000000", i.coord||[0,0,0]);
+}
 
-    export async function exportScene(): Promise<Scene>{
-        return scene;
-    }
-};
-
-export default HTMLHedron;
+export async function exportScene(): Promise<Scene>{
+    return scene;
+}
